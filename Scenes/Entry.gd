501df5f1c9359f85f177396e02f7419f74bd8a80
Ryan Mirch <rwmirch@gmail.com>
2019-10-15 16:07:31 -0400

    Refined First Header Row
    - All 5 entries come form an object
    - When exporting, entry text goes away revealing label text

---
+++ Scenes/Entry.gd
@@ -0,0 +1,17 @@
+extends LineEdit
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.connect("text_changed", self, "_update_label")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _update_label(newText):
+	get_parent().get_node("Entry Text").text = newText
