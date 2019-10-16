6766f675812fbe3db90ab91d2534068489460468
Ryan Mirch <rwmirch@gmail.com>
2019-10-16 03:01:41 -0400

    Finished Small Row Segments
    Shape menu still needs to be implemented.

--- Scenes/Entry.gd
+++ Scenes/Entry.gd
@@ -7,7 +7,7 @@ extends LineEdit
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.connect("text_changed", self, "_update_label")
-	max_length = $"../..".rect_size.x / 11.5
+	max_length = $"../..".rect_size.x / 6
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
