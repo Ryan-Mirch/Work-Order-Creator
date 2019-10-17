485eb77e248ad6034d0571ed140891e7d22cd863
Ryan Mirch <rwmirch@gmail.com>
2019-10-17 02:02:02 -0400

    Edit Button
    - there is now an edit button that brings up a popup menu  which will allow you to edit the shapes.

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -3,11 +3,38 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
 # Called when the node enters the scene tree for the first time.
+
+signal editing
+signal exporting
+signal popup
+
 func _ready():
-	pass # Replace with function body.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+	
+func _editing_start():
+	for lineEdit in get_tree().get_nodes_in_group("Editor Only"):
+		lineEdit.visible = true
+		
+	for lineEdit in get_tree().get_nodes_in_group("Export Only"):
+		lineEdit.visible = false
+		
+	for viewport in get_tree().get_nodes_in_group("Viewport"):
+		viewport.gui_disable_input = false
+	
+func _exporting_start():
+	for lineEdit in get_tree().get_nodes_in_group("Editor Only"):
+		lineEdit.visible = false
+		
+	for popup in get_tree().get_nodes_in_group("Popup"):
+		popup.visible = false
+		
+	for viewport in get_tree().get_nodes_in_group("Viewport"):
+		viewport.gui_disable_input = true
+	
+func _popup_start():
+	pass
