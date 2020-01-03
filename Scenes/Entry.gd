3906c0273d2353c163c67d414e3e5ae45fbce0e9
Ryan <rwmirch@gmail.com>
2020-01-02 20:28:45 -0500

    P0#, Materials, and Notes editable
    there is now a toggle to edit these 3 segments. Wether or not they are editable is saved.
    
    When editable, the first page has no effect on them.

--- Scenes/Entry.gd
+++ Scenes/Entry.gd
@@ -4,29 +4,40 @@ extends LineEdit
 # var a = 2
 # var b = "text"
 onready var pageInstance = $"../../../../../../"
+onready var sceneRoot = $"../.."
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.connect("text_changed", self, "_update_label")
 	max_length = $"../..".rect_size.x / 6
 	_update_label(text)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _update_label(newText):
 	if Global.state != 1: return
+	
 	$"../Text".text = newText
-	if pageInstance.get_index() != 0:#segment is not on page 1
+	
+	if pageInstance.get_index() != 0 && sceneRoot.editable:#segment is not on page 1 and it was changed
+		Global._save()
+		return
+	
+	
+	if pageInstance.get_index() != 0:#segment is not on page 1 and page 1 was changed
 		for seg in get_tree().get_nodes_in_group("Header Segment"):
 			if seg.get_parent().get_parent().get_parent().get_parent().get_index() == 0:
-				if seg.name == $"../..".name && seg != $"../..":
+				if seg.name == $"../..".name && seg != $"../.." && seg.editable == false:
 					_set_text(seg._get_text())
 				
 	else:#segment is on page 1
 		for seg in get_tree().get_nodes_in_group("Header Segment"):
-			if seg.name == $"../..".name && seg != $"../..":
+			if seg.name == $"../..".name && seg != $"../.." && seg.editable == false:
 				seg._set_text(newText) #set text to what this text is.
+				
 	
+					
 	Global._save()
 	
 func _set_text(s):
