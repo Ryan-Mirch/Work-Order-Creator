bf4070d5c04aa5f09b09c0f0ee244e1a82b1b51d
Ryan Mirch <rwmirch@gmail.com>
2022-02-17 06:50:52 -0500

    prevent row overflow, changed radio button to toggle

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -25,6 +25,9 @@ func _editing_start():
 	for lineEdit in get_tree().get_nodes_in_group("Export Only"):
 		lineEdit.visible = false
 		
+	for visuals in get_tree().get_nodes_in_group("Export Only Visuals"):
+		visuals.visible = false
+		
 	for viewport in get_tree().get_nodes_in_group("Viewport"):
 		viewport.gui_disable_input = false
 		
@@ -37,6 +40,9 @@ func _exporting_start():
 	state = 2
 	for lineEdit in get_tree().get_nodes_in_group("Editor Only"):
 		lineEdit.visible = false
+		
+	for visuals in get_tree().get_nodes_in_group("Export Only Visuals"):
+		visuals.visible = true
 		
 	for popup in get_tree().get_nodes_in_group("Popup"):
 		popup.visible = false
